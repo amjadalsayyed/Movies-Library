cb88749af277b2371c79e25b16429d7af76da9aa
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-03-14 11:58:42 +0300

    ee

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const client = new pg.Client(process.env.DATABASE_URL);
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.get("/", getData);
 app.get("/favorite", (req, res) => res.send("Hello from favorite"));
