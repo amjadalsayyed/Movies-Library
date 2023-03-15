19dacee69474caee7c7e6ed45f0bf8084aae7077
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-03-15 17:30:53 +0300

    return data from update

--- schema.sql
+++ schema.sql
@@ -4,8 +4,10 @@ CREATE TABLE IF NOT EXISTS Movies (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     release_date VARCHAR(255),
-    overview VARCHAR(10000),
+    overview VARCHAR(100000),
     poster_path VARCHAR(10000),
     comment VARCHAR(10000)
 );
 -- sqlserver://localhost:5432
+
+-- INSERT INTO movies (title, release_date, overview,poster_path,comment) VALUES (to_json('${movie.title}'),'${movie.release_date}',to_json('${movie.overview}'),'${movie.poster_path}','${movie.comment}')
